7ab2dfc223ea4a8a81cffa60d033cfb583930c17
vshobhit <vermashobhit1994@gmail.com>
2024-06-30 08:26:58 +0530

    refractor code for readability

--- slider/slider.js
+++ slider/slider.js
@@ -5,16 +5,21 @@ const sliderWindow = document.querySelector(".slider");
 let sliderNum = 1;
 let imagesLength = document.querySelectorAll(".image").length;
 
+let nextSlide = () =>{
+	sliderWindow.style.transform = `translate(${-800 * sliderNum}px)`;
+	sliderNum++;
+
+}
+
+let previousSlide = () => {
+	sliderNum = 1;
+	sliderWindow.style.transform = `translate(0px)`;
+
+}
+
 rightArrow.addEventListener("click", ()=>{
 	console.log("clicked");
 	
-	if(sliderNum < imagesLength){
-		sliderWindow.style.transform = `translate(${-800 * sliderNum}px)`;
-		sliderNum++;
-	}
-	else{
-		sliderNum = 1;
-		sliderWindow.style.transform = `translate(0px)`;
-	}
-	
+	sliderNum < imagesLength ? nextSlide() : previousSlide();
 })
+
