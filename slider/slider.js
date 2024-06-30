34c7b24b9241fb44b3140b70a45c7778fba7a623
vshobhit <vermashobhit1994@gmail.com>
2024-06-30 08:20:34 +0530

    change images when click right arrow

--- slider/slider.js
+++ slider/slider.js
@@ -1,1 +1,20 @@
-console.log("test js")
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+const sliderWindow = document.querySelector(".slider");
+
+let sliderNum = 1;
+let imagesLength = document.querySelectorAll(".image").length;
+
+rightArrow.addEventListener("click", ()=>{
+	console.log("clicked");
+	
+	if(sliderNum < imagesLength){
+		sliderWindow.style.transform = `translate(${-800 * sliderNum}px)`;
+		sliderNum++;
+	}
+	else{
+		sliderNum = 1;
+		sliderWindow.style.transform = `translate(0px)`;
+	}
+	
+})
