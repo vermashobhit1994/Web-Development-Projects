35caa3216289a74115d73d93cb6e08a6a44c31a6
vermashobhit1994 <vermashobhit1994@gmail.com>
2026-04-11 19:51:12 +0530

    implement light dark mode switch functionaity with localstorage

--- projects/frontend-mentor/01_Rest-countries-API/country.js
+++ projects/frontend-mentor/01_Rest-countries-API/country.js
@@ -1,3 +1,4 @@
+
 // back button implementation
 const backBtn = document.querySelector(".back-btn")
 backBtn.href = "#"
@@ -72,7 +73,7 @@ const countriesNativeLanguages = {
 }
 
 
-isRequestSent = false
+let isRequestSent = false
 
 if(!isRequestSent){
     fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=True`)
@@ -138,7 +139,7 @@ if(!isRequestSent){
                 const [nativeName] = nativeNames
                 countryNativeName.append(`${nativeName.common}`)
             }else if(country.name.common){
-                for (language of Object.keys(countriesNativeLanguages)){
+                for (const language of Object.keys(countriesNativeLanguages)){
                         if(country.name.common == language)
                             singleNativeLanguage = countriesNativeLanguages[language]
                     }
@@ -215,7 +216,7 @@ if(!isRequestSent){
             countryCapital.append(boldText)
             const capitalsSorted = country.capital.toSorted()
             const lastCapitalItem = capitalsSorted.at(-1)
-            for(capital of capitalsSorted){
+            for(const capital of capitalsSorted){
                 if(capital != lastCapitalItem){
                     countryCapital.append(`${capital}, `)
                 }else{
@@ -252,7 +253,7 @@ if(!isRequestSent){
             
             const currenciesSorted = Object.values(country.currencies).toSorted()
             
-            for (currencyCodeValue of currenciesSorted){
+            for (const currencyCodeValue of currenciesSorted){
                 if(currenciesSorted.length == 1){
                     countryCurrencyName.innerText = currencyCodeValue?.name
                 }else{
@@ -277,7 +278,7 @@ if(!isRequestSent){
             countryLanguages.append(boldText)
             const languagesSorted = Object.values(country.languages).toSorted()
             const lastLanguageItem = languagesSorted.at(-1) 
-            for (languages of languagesSorted){
+            for (const languages of languagesSorted){
                 if (languages != lastLanguageItem){
                     countryLanguages.append(`${languages}, `)
                 }else{
@@ -309,7 +310,7 @@ if(!isRequestSent){
 
             const borderCountriesCodes = country.borders
             
-            for(countryCode of borderCountriesCodes){
+            for(const countryCode of borderCountriesCodes){
                 
                 
                 // country border names using country codes
@@ -344,5 +345,61 @@ if(!isRequestSent){
 
     isRequestSent = true
 }
+
+
+function changeTheme(){
+        const sunIconString = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="hsl(0, 100%, 100%)"><path d="M440-760v-160h80v160h-80Zm266 110-55-55 112-115 56 57-113 113Zm54 210v-80h160v80H760ZM440-40v-160h80v160h-80ZM254-652 140-763l57-56 113 113-56 54Zm508 512L651-255l54-54 114 110-57 59ZM40-440v-80h160v80H40Zm157 300-56-57 112-112 29 27 29 28-114 114Zm113-170q-70-70-70-170t70-170q70-70 170-70t170 70q70 70 70 170t-70 170q-70 70-170 70t-170-70Zm283-57q47-47 47-113t-47-113q-47-47-113-47t-113 47q-47 47-47 113t47 113q47 47 113 47t113-47ZM480-480Z"/></svg>';
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(sunIconString, "image/svg+xml");
+    const sunIcon = doc.documentElement;
+
+    const moonIcon = document.querySelector('.header-content button svg')
+    const themeBtn = document.querySelector(".header-content button")
+    
+    const darkModeBtnText = document.createTextNode('\u00A0\u00A0Dark Mode')
+    const lightModeBtnText = document.createTextNode('\u00A0\u00A0Light Mode')
+    if (!localStorage.getItem("colorTheme"))
+        localStorage.setItem("colorTheme", "light");
+    
+    else if(localStorage.getItem("colorTheme") == "dark")
+        document.body.classList.add("dark");
+            
+    
+
+    
+
+    themeBtn.addEventListener("click", (e) => {
+        console.log(localStorage.getItem("colorTheme"))
+        document.body.classList.toggle("dark");
+
+        if(localStorage.getItem("colorTheme") == "light"){
+            moonIcon.remove();
+            themeBtn.innerText = "";
+            themeBtn.prepend(sunIcon);
+            themeBtn.appendChild(lightModeBtnText);
+
+            localStorage.setItem("colorTheme", "dark");
+        }else if(localStorage.getItem("colorTheme") == "dark"){
+            sunIcon.remove();
+            themeBtn.innerText = "";
+            themeBtn.prepend(moonIcon);
+            themeBtn.appendChild(darkModeBtnText);
+            localStorage.setItem("colorTheme", "light");
+        }
+        
+    });
+}
+
+changeTheme()
+
+
+
+// window.addEventListener("storage", (e) => {
+//     // window.localStorage.setItem(`colorTheme`, mode)
+//     console.log(window.localStorage.getItem(`1`))
+//     window.localStorage.setItem("1", 'world')
+    
+// })
+
 
 
--- projects/frontend-mentor/01_Rest-countries-API/script.js
+++ projects/frontend-mentor/01_Rest-countries-API/script.js
@@ -1,22 +1,21 @@
 const countriesContainer = document.querySelector(".countries-container");
-const title =document.querySelector('.title')
-const searchCountryDarkMode = document.querySelector(".search-country")
-const filterByRegionDarkMode = document.querySelector(".filter-by-region")
-
+const title = document.querySelector(".title");
+const searchCountryDarkMode = document.querySelector(".search-country");
+const filterByRegionDarkMode = document.querySelector(".filter-by-region");
 
 let allCountriesData = null;
 
 fetch(
     "https://restcountries.com/v3.1/all?fields=capital,flags,continents,name,population,cca3",
-).then((response) => response.json())
-.then((data) => {
-    allCountriesData = data;
-    renderCountries(data)
-});
+)
+    .then((response) => response.json())
+    .then((data) => {
+        allCountriesData = data;
+        renderCountries(data);
+    });
 
 function renderCountries(data) {
     data.forEach((country) => {
-
         // Each country card contains
         // 1. flag
         // 2. common name
@@ -71,8 +70,8 @@ function renderCountries(data) {
         countryCapital.appendChild(boldText);
 
         // add , to each country capitals excluding last country
-        lastCapitalIndex = country.capital.length - 1;
-        capitals = "";
+        const lastCapitalIndex = country.capital.length - 1;
+        const capitals = "";
         if (country.capital.length > 1) {
             for (const [index, item] of country.capital.entries()) {
                 if (index != lastCapitalIndex) {
@@ -92,109 +91,110 @@ function renderCountries(data) {
         );
 
         countryCard.append(imgContainer, cardText);
-        countriesContainer.appendChild(countryCard);
+        if (countriesContainer) {
+            countriesContainer.appendChild(countryCard);
+        }
     });
 }
 
-
-function searchCountries(){
-    
+function searchCountries() {
+    const countriesContainer = document.querySelector(".countries-container");
     const searchInput = document.querySelector(".search-country input");
-    
-    searchInput.addEventListener("input", (e) => {
-        const searchedValue = e.target.value.toLowerCase();
-        
-        
-    
-        if(allCountriesData){
-            const filteredCountries = allCountriesData.filter((country) => {
-                return country.name.common.toLowerCase().includes(searchedValue)
-            })
-            
-            // clear previous output 
-            countriesContainer.innerHTML = ""
-            renderCountries(filteredCountries)
 
-            // show message no country found
-            if(filteredCountries.length == 0){
-                const countriesNotFound = document.createElement('p')
-                countriesNotFound.classList.add("countries-not-found")
-                countriesNotFound.innerText = "No country found for matching criteria"
-                countriesContainer.appendChild(countriesNotFound)            
+    if (searchInput) {
+        searchInput.addEventListener("input", (e) => {
+            const searchedValue = e.target.value.toLowerCase();
+
+            if (allCountriesData) {
+                const filteredCountries = allCountriesData.filter((country) => {
+                    return country.name.common
+                        .toLowerCase()
+                        .includes(searchedValue);
+                });
+
+                // clear previous output
+                countriesContainer.innerHTML = "";
+                renderCountries(filteredCountries);
+
+                // show message no country found
+                if (filteredCountries.length == 0) {
+                    const countriesNotFound = document.createElement("p");
+                    countriesNotFound.classList.add("countries-not-found");
+                    countriesNotFound.innerText =
+                        "No country found for matching criteria";
+                    countriesContainer.appendChild(countriesNotFound);
+                }
             }
-        }
-        
-        
-    });
+        });
+    }
 }
 
-function filterByRegion(){
-    const filterByRegion = document.querySelector('.filter-by-region')
-    filterByRegion.addEventListener('change', (e) => {
-        if(e.target.value){
-            // search by region using API
-            fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
-            .then((resp) => resp.json())
-            .then((data) => {
-                // clear previous output 
-                countriesContainer.innerHTML = ""
-                renderCountries(data)
-            })
-        }
-        
-    })
+function filterByRegion() {
+    const filterByRegion = document.querySelector(".filter-by-region");
+    if (filterByRegion) {
+        filterByRegion.addEventListener("change", (e) => {
+            if (e.target.value) {
+                // search by region using API
+                fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
+                    .then((resp) => resp.json())
+                    .then((data) => {
+                        // clear previous output
+                        countriesContainer.innerHTML = "";
+                        renderCountries(data);
+                    });
+            }
+        });
+    }
 }
 
-
-function changeColorTheme(){
-    
-    const sunIconString = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="hsl(0, 100%, 100%)"><path d="M440-760v-160h80v160h-80Zm266 110-55-55 112-115 56 57-113 113Zm54 210v-80h160v80H760ZM440-40v-160h80v160h-80ZM254-652 140-763l57-56 113 113-56 54Zm508 512L651-255l54-54 114 110-57 59ZM40-440v-80h160v80H40Zm157 300-56-57 112-112 29 27 29 28-114 114Zm113-170q-70-70-70-170t70-170q70-70 170-70t170 70q70 70 70 170t-70 170q-70 70-170 70t-170-70Zm283-57q47-47 47-113t-47-113q-47-47-113-47t-113 47q-47 47-47 113t47 113q47 47 113 47t113-47ZM480-480Z"/></svg>';
+export function changeColorTheme() {
+    const sunIconString =
+        '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="hsl(0, 100%, 100%)"><path d="M440-760v-160h80v160h-80Zm266 110-55-55 112-115 56 57-113 113Zm54 210v-80h160v80H760ZM440-40v-160h80v160h-80ZM254-652 140-763l57-56 113 113-56 54Zm508 512L651-255l54-54 114 110-57 59ZM40-440v-80h160v80H40Zm157 300-56-57 112-112 29 27 29 28-114 114Zm113-170q-70-70-70-170t70-170q70-70 170-70t170 70q70 70 70 170t-70 170q-70 70-170 70t-170-70Zm283-57q47-47 47-113t-47-113q-47-47-113-47t-113 47q-47 47-47 113t47 113q47 47 113 47t113-47ZM480-480Z"/></svg>';
     const parser = new DOMParser();
     const doc = parser.parseFromString(sunIconString, "image/svg+xml");
     const sunIcon = doc.documentElement;
 
-    const moonIcon = document.querySelector('.header-content button svg')
-    const themeBtn = document.querySelector(".header-content button")
+    const moonIcon = document.querySelector(".header-content button svg");
+    const themeBtn = document.querySelector(".header-content button");
+
+    const darkModeBtnText = document.createTextNode("\u00A0\u00A0Dark Mode");
+    const lightModeBtnText = document.createTextNode("\u00A0\u00A0Light Mode");
+
     
-    const darkModeBtnText = document.createTextNode('\u00A0\u00A0Dark Mode')
-    const lightModeBtnText = document.createTextNode('\u00A0\u00A0Light Mode')
+    if (!localStorage.getItem("colorTheme"))
+        localStorage.setItem("colorTheme", "light");
     
-    const searchIcon = document.querySelector(".search-country svg")
-    const searchIconDarkModeColor = 'hsl(0, 100%, 100%)'
-    const searchIconLighModeColor = 'hsl(200, 15%, 8%)'
-
-
-    let isDarkMode = false
-
+    else if(localStorage.getItem("colorTheme") == "dark")
+        document.body.classList.add("dark");
+    
     themeBtn.addEventListener("click", (e) => {
-        countriesContainer.classList.toggle("dark")
-        document.body.classList.toggle('dark')
-        document.querySelector(".search-country").classList.toggle('dark')
-        document.querySelector(".filter-by-region").classList.toggle('dark')
-        document.querySelector(".header-content").classList.toggle('dark')
+        console.log(localStorage.getItem("colorTheme"))
+        document.body.classList.toggle("dark");
 
-        if(!isDarkMode){
-            moonIcon.remove()
-            themeBtn.innerText = ""
-            themeBtn.prepend(sunIcon)
-            themeBtn.appendChild(lightModeBtnText)
-            searchIcon.style.fill = searchIconDarkModeColor
-            isDarkMode = true
-                        
-        }else{
-            sunIcon.remove()
-            themeBtn.innerText = ""
-            themeBtn.prepend(moonIcon)
-            themeBtn.appendChild(darkModeBtnText)
-            searchIcon.style.fill = searchIconLighModeColor
-            isDarkMode = !isDarkMode
+        if(localStorage.getItem("colorTheme") == "light"){
+            moonIcon.remove();
+            themeBtn.innerText = "";
+            themeBtn.prepend(sunIcon);
+            themeBtn.appendChild(lightModeBtnText);
+
+            localStorage.setItem("colorTheme", "dark");
+        }else if(localStorage.getItem("colorTheme") == "dark"){
+            sunIcon.remove();
+            themeBtn.innerText = "";
+            themeBtn.prepend(moonIcon);
+            themeBtn.appendChild(darkModeBtnText);
+            localStorage.setItem("colorTheme", "light");
         }
         
-        
-        
-    })
+    });
+    
 }
 
-searchCountries()
-filterByRegion()
-changeColorTheme()
+searchCountries();
+filterByRegion();
+changeColorTheme();
+
+// const themeBtn = document.querySelector(".header-content button")
+// themeBtn.addEventListener('click', (e) => {
+//     document.body.classList.toggle('dark')
+// })
