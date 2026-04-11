f29558d501f56b8055a7ce66c6498f81363efbc9
vermashobhit1994 <vermashobhit1994@gmail.com>
2026-04-11 15:37:20 +0530

    implement light dark mode button functionality

--- projects/frontend-mentor/01_Rest-countries-API/script.js
+++ projects/frontend-mentor/01_Rest-countries-API/script.js
@@ -1,6 +1,7 @@
 const countriesContainer = document.querySelector(".countries-container");
 const title =document.querySelector('.title')
-
+const searchCountryDarkMode = document.querySelector(".search-country")
+const filterByRegionDarkMode = document.querySelector(".filter-by-region")
 
 
 let allCountriesData = null;
@@ -15,14 +16,6 @@ fetch(
 
 function renderCountries(data) {
     data.forEach((country) => {
-        
-        
-        // if (country?.name?.nativeName) {
-        //     const nativeNames = Object.values(country?.name?.nativeName);
-        //     if (Object.keys(nativeNames).length > 1) {
-        //         // console.log(country);
-        //     }
-        // }
 
         // Each country card contains
         // 1. flag
@@ -103,16 +96,6 @@ function renderCountries(data) {
     });
 }
 
-// const countryCard = document.createElement('a')
-// countryCard.classList.add('country-card')
-
-// console.log(countryCard)
-
-// flagImage = document.createElement('img')
-// flagImage.setAttribute("src","https://")
-// flagImage.setAttribute("alt","Germany country")
-
-// countryCard.appendChild(flagImage)
 
 function searchCountries(){
     
@@ -156,7 +139,6 @@ function filterByRegion(){
                 // clear previous output 
                 countriesContainer.innerHTML = ""
                 renderCountries(data)
-                console.log(data)
             })
         }
         
@@ -164,6 +146,55 @@ function filterByRegion(){
 }
 
 
+function changeColorTheme(){
+    
+    const sunIconString = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="hsl(0, 100%, 100%)"><path d="M440-760v-160h80v160h-80Zm266 110-55-55 112-115 56 57-113 113Zm54 210v-80h160v80H760ZM440-40v-160h80v160h-80ZM254-652 140-763l57-56 113 113-56 54Zm508 512L651-255l54-54 114 110-57 59ZM40-440v-80h160v80H40Zm157 300-56-57 112-112 29 27 29 28-114 114Zm113-170q-70-70-70-170t70-170q70-70 170-70t170 70q70 70 70 170t-70 170q-70 70-170 70t-170-70Zm283-57q47-47 47-113t-47-113q-47-47-113-47t-113 47q-47 47-47 113t47 113q47 47 113 47t113-47ZM480-480Z"/></svg>';
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(sunIconString, "image/svg+xml");
+    const sunIcon = doc.documentElement;
+
+    const moonIcon = document.querySelector('.header-content button svg')
+    const themeBtn = document.querySelector(".header-content button")
+    
+    const darkModeBtnText = document.createTextNode('\u00A0\u00A0Dark Mode')
+    const lightModeBtnText = document.createTextNode('\u00A0\u00A0Light Mode')
+    
+    const searchIcon = document.querySelector(".search-country svg")
+    const searchIconDarkModeColor = 'hsl(0, 100%, 100%)'
+    const searchIconLighModeColor = 'hsl(200, 15%, 8%)'
+
+
+    let isDarkMode = false
+
+    themeBtn.addEventListener("click", (e) => {
+        countriesContainer.classList.toggle("dark")
+        document.body.classList.toggle('dark')
+        document.querySelector(".search-country").classList.toggle('dark')
+        document.querySelector(".filter-by-region").classList.toggle('dark')
+        document.querySelector(".header-content").classList.toggle('dark')
+
+        if(!isDarkMode){
+            moonIcon.remove()
+            themeBtn.innerText = ""
+            themeBtn.prepend(sunIcon)
+            themeBtn.appendChild(lightModeBtnText)
+            searchIcon.style.fill = searchIconDarkModeColor
+            isDarkMode = true
+                        
+        }else{
+            sunIcon.remove()
+            themeBtn.innerText = ""
+            themeBtn.prepend(moonIcon)
+            themeBtn.appendChild(darkModeBtnText)
+            searchIcon.style.fill = searchIconLighModeColor
+            isDarkMode = !isDarkMode
+        }
+        
+        
+        
+    })
+}
 
 searchCountries()
 filterByRegion()
+changeColorTheme()
